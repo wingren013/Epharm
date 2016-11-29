34fab88ae6affd7711154596942e360cd0eff85a
Sawyer Mifsud <wingren013@gmail.com>
2016-11-29 11:14:59 -0800

    Epharm initial

---
+++ IdeaProjects/epharm_server/src/myserverstarter.java
@@ -0,0 +1,20 @@
+/**
+ * Created by smifsud on 11/29/16.
+ */
+public class myserverstarter {
+    static Integer listen = null;
+
+    public myserverstarter() {
+        try {
+            init();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void init() throws Exception
+    {
+
+    }
+}
